9f6ff02a85de5083b4a41325b76c18f4277eda81
Matatias <matatias97@gmail.com>
2022-08-25 13:42:40 +0700

    chore: change color

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.teal
+        primarySwatch: Colors.lightBlue
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
